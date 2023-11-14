d05553596e644c130170e7f3c452a6b24e96777f
Ryan Djemtchemeu <ryanmambou@gmail.com>
2023-11-14 16:37:34 +0100

    project

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'assets.pokemon.com',
+            port: '',
+            pathname: '/assets/**',
+          },
+        ],
+        domains: ['pokeapi.co', 'assets.pokemon.com'],
+        },
+}
+
+module.exports = nextConfig
